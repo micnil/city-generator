f47b89307e4433f2ba2a9e3a7713def5830aaef7
micnil <michaelnilsson90@hotmail.com>
2016-02-13 12:46:04 +0100

    included dat.gui, removed unnesesary uniforms for buildings

--- shaders/building-chunk.frag
+++ shaders/building-chunk.frag
@@ -8,8 +8,9 @@ vec3 roofColor = vec3(uRoofColor);
 vec3 position = vec3(vPosition);
 
 wallColor += wallColor * snoise(vUv/3.0);
-float xCoord = (absNormal.x > 0.5) ? position.y + dimensions.y / 2.0 : position.x + dimensions.x / 2.0;
-position.z += dimensions.z / 2.0;
+//float xCoord = (absNormal.x > 0.5) ? position.y + dimensions.y / 2.0 : position.x + dimensions.x / 2.0;
+float xCoord = (absNormal.x > 0.5) ? position.y: position.x;
+//position.z += dimensions.z / 2.0;
 float isWindow = mod(floor(position.z / windowHeight), 2.0);
 float windowCoord = fract(xCoord / windowWidth);
 float isWindowEdge = (windowCoord < 0.3) ? 1.0 : 0.0;
--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
 import THREE from 'three';
 import OrbitControls from 'three-orbit-controls';
 import CityGenerator from 'src/city-generator';
+import dat from 'datgui';
 var scene = new THREE.Scene();
 //scene.fog = new THREE.FogExp2( 0xAAAAAA, 0.25 );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var gui = new dat.GUI();
+console.log(gui);
 
-var renderer = new THREE.WebGLRenderer();
+var menu = {
+	hej: "hej"
+}
+
+gui.add(menu, 'hej')
+//menu.hej = "då";
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var cityGenerator = CityGenerator();
-var city = cityGenerator.generate(15, 15);
+var city = cityGenerator.generate(6, 6);
 scene.add( city );
 city.rotation.x -= Math.PI / 4;
 var Controls = OrbitControls(THREE);
 var controls = new Controls( camera, renderer.domElement );
+controls.addEventListener( 'change', render );
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
 controls.enableZoom = true;
@@ -31,10 +41,11 @@ scene.add( directionalLight );
 
 camera.position.z = 4;
 
-var render = function () {
-	requestAnimationFrame( render );
+function render() {
+	//requestAnimationFrame( render );
 	controls.update();
 	renderer.render(scene, camera);
+	console.log(menu.hej)
 };
 
 render();
